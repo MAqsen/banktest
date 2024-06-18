ff989f412cd5255a6f1c0635d19a37ccd7d1655e
MAqsen <maxencemalherre@gmail.com>
2024-06-18 22:57:32 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,33 +3,33 @@ import pandas as pd
 
 # Fonction de prétraitement des données
 def preprocess_data(bank):
-    st.write("### Suite à ces analyses nous pouvons passer au pré-processing du jeu de données.")
+    st.write("Suite à ces analyses nous pouvons passer au pré-processing du jeu de données.")
     
-    st.write("### Comme vu précédemment, le jeu de données est propre car il ne contient aucun doublon ni valeurs manquantes. Il dispose malgré tout de nombreuses valeurs insignifiantes tel que 'unknown'(11239) et 'others'(537). Nous avons décidé de supprimer la valeur 'unknown' des variables 'job' et 'education' car cela n'impactera pas le dataset au vu du faible volume de cette valeur.")
+    st.write("Comme vu précédemment, le jeu de données est propre car il ne contient aucun doublon ni valeurs manquantes. Il dispose malgré tout de nombreuses valeurs insignifiantes tel que 'unknown'(11239) et 'others'(537). Nous avons décidé de supprimer la valeur 'unknown' des variables 'job' et 'education' car cela n'impactera pas le dataset au vu du faible volume de cette valeur.")
     
     # Suppression des lignes avec les valeurs 'unknown' pour les colonnes 'job' et 'education'
     bank_cleaned = bank.drop(bank.loc[bank["job"] == "unknown"].index, inplace=False)
     bank_cleaned = bank_cleaned.drop(bank_cleaned.loc[bank_cleaned["education"] == "unknown"].index, inplace=False)
 
-    st.write("### Nous avons eu une réflexion pour certaines variables :")
+    st.write("Nous avons eu une réflexion pour certaines variables :")
     
-    st.write("#### - poutcome")
+    st.write("- poutcome")
     st.write("Nous avons réfléchis à 3 options :")
     st.write("1. soit nous gardons cette variable dans le dataset et nous supprimons les lignes 'unknown'. Cela a pour conséquence de réduire considérablement la taille de notre dataset. Mais nous serons certainement amenés à le réduire dans tous les cas par la suite.")
     st.write("2. soit nous la gardons telle quelle. Nous pouvons choisir un modèle qui peut être entraîné avec ce type de donnée, et nous verrons l’impact.")
     st.write("3. soit nous supprimons complètement cette colonne car la distribution pourrait impacter négativement notre modèle.")
     st.write("Nous sommes plutôt partis sur la deuxième solution, car outre les 'unknown' et 'other', la distribution de la variable est plutôt bonne..")
 
-    st.write("#### - contact")
+    st.write("- contact")
     st.write("Nous avons décidé de supprimer cette colonne car sa distribution n’est pas représentative.")
 
-    st.write("#### - pdays")
+    st.write("- pdays")
     st.write("Nous avons décidé de supprimer cette colonne à cause de la valeur -1 sur-représentée et que nous ne sommes pas sûrs de bien interpréter.")
     
-    # Suppression des colonnes 'contact' et 'pdays' car non significatives
+    st.write(Suppression des colonnes 'contact' et 'pdays' car non significatives)
     bank_cleaned = bank_cleaned.drop(['contact', 'pdays'], axis=1)
     
-    st.write("### Nous avons également transformé la durée en minute sur Duration.")
+    st.write("Nous avons également transformé la durée en minute sur Duration.")
     # Transformation de la durée en minutes
     bank_cleaned['duration'] = bank_cleaned['duration'] // 60
     
